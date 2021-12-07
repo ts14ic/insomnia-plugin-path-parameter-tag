d77aa16166bb279da1556570f4cc777da0fceacb
Artyom Yazinsky <iw2snl@gmail.com>
2021-12-07 18:06:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Path parameter tag Insomnia plugin
+
+A template tag, that can be used inside URIs to as path parameters.  
+
+![uri preview with display name](./screenshots/uri-preview-with-display-name.png)
+
+## Usage
+
+Add the plugin to Insomnia by its name: "insomnia-plugin-path-parameter-tag".  
+
+Create/edit a request, and when you want a path parameter:
+* either press ctrl+space to insert the "Path parameter" tag
+* or type `{% pathParam %}`, which does the same.
+
+Here's an example of a URI with the value of `https://example.com/{% pathParam %}`.  
+![empty tag](./screenshots/empty.png)
+
+Click on the tag to edit its Value and optionally Display name.  
+![edit dialog](./screenshots/edit-dialog.png)
+
+Only "Value" is required - this will be sent with the request.
+Here's the URI and CURL previews with the request above, and a value set:  
+![uri preview](./screenshots/uri-preview-with-value.png)
+![curl preview](./screenshots/curl-preview.png)
+
+"Description" is not sent anywhere, and is there only for documentation purpose.  
+
+"Display name" is also not sent anywhere, but you can use it to configure the look of the tag Insomnia.  
+![uri preview with display name](./screenshots/uri-preview-with-display-name.png)
---
+++ main.js
@@ -0,0 +1,41 @@
+const log = (...args) => {
+  if (log.enabled) {
+    console.log("[path-parameter-tag]", ...args);
+  }
+};
+log.enabled = false;
+
+const pathParameterTag = {
+    name: "pathParam",
+    displayName: "Path parameter",
+    liveDisplayName(args) {
+      const name = args[0].value;
+      return name ? name : args[1].value;
+    },
+    description: "replace part of URI path with given value",
+    args: [
+      {
+        displayName: "Display name",
+        description: "Optional name to display instead of value in Insomnia UI. Doesn't affect the request made.",
+        type: "string"
+      }, {
+        displayName: "Value",
+        description: "Value of the path parameter",
+        type: "string"
+      }, {
+        displayName: "Description",
+        description: "Optional description of this parameter, which doesn't affect the request made",
+        type: "string"
+      }, {
+        displayName: "URI Encode",
+        description: "Select to URI encode the parameter",
+        type: "boolean",
+        defaultValue: true
+      }
+    ],
+    async run(context, name, value, description, mustEncode) {
+      return mustEncode ? encodeURIComponent(value) : value;
+    }
+};
+
+module.exports.templateTags = [pathParameterTag];
